9376571c559b3f3b66881eb146b9af5fe178f5fe
JosephKevinMachado <josephkevinmachado@pop-os.fios-router.home>
2024-06-03 13:56:12 -0400

    2024-06-03-13-56-12 - dq-checks

---
+++ 4-data-quality.py
@@ -0,0 +1,15 @@
+import polars as pl
+from cuallee import Check, CheckLevel
+
+# Read CSV file into Polars DataFrame
+df = pl.read_csv("./data/sample_data.csv")
+
+# Nulls on column Id
+check = Check(CheckLevel.ERROR, "Completeness")
+validation_results_df = (
+    check.is_complete("Customer_ID").is_unique("Customer_ID").validate(df)
+)
+print(validation_results_df)
+
+results = validation_results_df["status"].to_list()
+assert "FAIL" not in results == True
